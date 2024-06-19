6ca47966b61f74964abbf45ae4941b42beb1c21c
Jisung15 <gimseongjin302@gmail.com>
2024-06-20 00:41:56 +0900

    키오스크 과제 Level4 수정 (피드백 적용 2차, 약간 문제가 있어서 다시 수정하고 올리는 버전)

--- app/src/main/java/com/example/kotlinproject/MainValue.kt
+++ app/src/main/java/com/example/kotlinproject/MainValue.kt
@@ -2,4 +2,4 @@ package com.example.kotlinproject
 
 data class MainValue (val name: String, val description: String, val subMenus: List<SubValue>)
 
-// 여기는 데이터 클래스
+// 여기도 메인 메뉴 관련 데이터 클래스
--- app/src/main/java/com/example/kotlinproject/SubValue.kt
+++ app/src/main/java/com/example/kotlinproject/SubValue.kt
@@ -2,4 +2,4 @@ package com.example.kotlinproject
 
 data class SubValue(val name: String, val price: Double, val comment: String)
 
-// 여기도 데이터 클래스
+// 여기는 서브 메뉴 관련 데이터 클래스
